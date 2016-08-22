b4858b064ce44375f10d3ff3b0d337a5fe14a9bb
Todd Coulson <tcoulson@gmail.com>
2016-08-22 16:44:04 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# udacity_movie_trailer_website" 
---
+++ entertainment_center.py
@@ -0,0 +1,13 @@
+import fresh_tomatoes
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg", "https://www.youtube.com/watch?v=4KPTXpQehio")
+rocky = media.Movie("Rocky", "A story of a man, trying to beat the odds", "https://upload.wikimedia.org/wikipedia/en/thumb/1/18/Rocky_poster.jpg/220px-Rocky_poster.jpg", "https://www.youtube.com/watch?v=3VUblDwa648")
+ferris = media.Movie("Ferris Bueller's Day Off", "A story of a boy who doesn't go to school.", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Ferris_Bueller%27s_Day_Off.jpg/220px-Ferris_Bueller%27s_Day_Off.jpg", "https://www.youtube.com/watch?v=ik0y8yWw7jU")
+shawshank = media.Movie("Shawshank Redemption", "A story of a man wrongly imprisoned for killing his wife.", "https://upload.wikimedia.org/wikipedia/en/thumb/8/81/ShawshankRedemptionMoviePoster.jpg/220px-ShawshankRedemptionMoviePoster.jpg", "https://www.youtube.com/watch?v=6hB3S9bIaco")
+starwars = media.Movie("Star Wars Rouge One", "The seventh installment of an epic story.", "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png/220px-Rogue_One%2C_A_Star_Wars_Story_poster.png", "https://www.youtube.com/watch?v=frdj1zb9sMY")
+batman = media.Movie("The Dark Knight", "A story of Batman dealing with the Joker.", "https://upload.wikimedia.org/wikipedia/en/thumb/8/8a/Dark_Knight.jpg/220px-Dark_Knight.jpg", "https://www.youtube.com/watch?v=_PZpmTj1Q8Q")
+
+movies=[toy_story, rocky, ferris, batman, starwars, shawshank]
+fresh_tomatoes.open_movies_page(movies)
+print(media.Movie.VALID_RATINGS)
